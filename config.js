2ecc7e5828b443fe97c7eafb5df598c079e4e246
hMatan <40565891+hMatan@users.noreply.github.com>
2025-02-09 12:33:37 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,28 @@
+const birthdayConfig = {
+    messages: [
+        "! יום הולדת שמח מתוק 🎉🎂",
+        ",היום הוא יום מיוחד",
+        ".לא סתם עוד יום רגיל",
+        "היום זהו יום ההולדת שלך",
+        "וחוגגים לך התחלה של שנה חדשה",
+        "שנה של שמחה כיף והמון הפתעות",
+        ",מאחלים לך את כל האושר שבעולם",
+        ",שהשנה הזו תהיה מלאה בחוויות מדהימות",
+        ",בהגשמת חלומות",
+        "! ובכל מה שגורם לך להיות מאושר",
+        "אוהבים אותך המון המון המון",
+        ":) ההירשביינים מהצפון"
+    ],
+    buttons: {
+        "turn_on": "תדליק את האור",
+        "play": "נגן מוסיקה",
+        "bannar_coming": "בוא נתחיל",
+        "balloons_flying": "טוס על גבי הבלונים",
+        "cake_fadein": "העוגה הכי מושקעת בעולם",
+        "light_candle": "תדליק את הנר",
+        "wish_message": "מזל טוב גונצ'וק",
+        "story": "ועכשיו לברכה"
+    },
+    balloons: ["*", "י", "נ", "ו", "ג", "*", "*"],
+    balloonColors: ["#F2B300", "#0719D4", "#D14D39", "#8FAD00", "#8377E4", "#99C96A", "#20CFB4"]
+};
